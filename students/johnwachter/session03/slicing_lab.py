ccb3e69eac61db461687ca88748d2815cf3663b3
johnwachter <johnwachter29@gmail.com>
2019-01-26 14:11:45 -0800

    running assert tests to make sure functions work

--- students/johnwachter/session03/slicing_lab.py
+++ students/johnwachter/session03/slicing_lab.py
@@ -10,26 +10,42 @@ def swap(string):
     return newstring
 
 swapped = swap("string")
+print("Swap String")
 print(swapped)
 assert swap("flipped") == "dlippef"
 print("Test passed")
-
+print("="*45)
 
 def remove(tupl):
     newtupl = tupl[0:-1:2]
-    print(newtupl)
+    return newtupl
 mytupl = (1,2,3,4,5,6,7,8,9)
-remove(mytupl)
+removeditems = remove(mytupl)
+print("Remove every other item")
+print(removeditems)
+print("="*45)
+
+def remove4everyother(string):
+    return string[4:-4:2]
+everyother4 = (1,2,3,4,5,'dontshow','show', 'dontshow', 'show', 6,7,8,9)
+print("first 4 and the last 4 items removed, and then every other item in the remaining sequence")
+print(remove4everyother(everyother4))
+print('='*45)
 
 def thirds(string):
-    thirds = len(string)/3
-    first = thirds
+    thirds = int(len(string)/3)
+    first = string[0:thirds]
+    last = string[-thirds:]
+    mid = string[thirds:-thirds]
+    return last + first + mid
+newthirds = thirds("111222333")
+print("last third, then first third, then the middle third in the new order")
+print(newthirds)
+print("="*45)
 
-string = "string"
-thirds = int(len(string)/3)
-first = string[0:thirds]
-print("first {}".format(first))
-last = string[-thirds:]
-print("last {}".format(last))
-mid = string[thirds:thirds]
-print("mid {}".format(mid))
+def reverse(string):
+    return string[::-1]
+reversedstring = reverse("reverse this string")
+print("Reverse a string")
+print(reversedstring)
+print("="*45)

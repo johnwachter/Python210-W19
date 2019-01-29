9bb7dfd8d024d747869982ee674dd99994544d0d
johnwachter <johnwachter29@gmail.com>
2019-01-28 19:33:18 -0800

    Built Donor database

--- students/johnwachter/mailroom/mailroom.py
+++ students/johnwachter/mailroom/mailroom.py
@@ -1,3 +1,10 @@
 #Title: mailroom.py
 #Change Log: (Who, When, What)
 #JWachter, 1/19/2019, creating file to upload to github
+#JWachter, 1/28/2019, built donor database
+
+donor_db = [("William Gates, III", [653772.32, 12.17]),
+            ("Jeff Bezos", [877.33]),
+            ("Paul Allen", [663.23, 43.87, 1.32]),
+            ("Mark Zuckerberg", [1663.23, 4300.87, 10432.0]),
+            ("Me Myself", [100])]

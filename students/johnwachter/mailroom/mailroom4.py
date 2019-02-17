a4cd2fc794184e363602ab557abe99236638239b
johnwachter <johnwachter29@gmail.com>
2019-02-16 19:32:35 -0800

    created two helper functions and put them inside the sendthankyou function to make that function do only one thing as opposed to 3

--- students/johnwachter/mailroom/mailroom4.py
+++ students/johnwachter/mailroom/mailroom4.py
@@ -2,7 +2,7 @@
 #Change Log: (Who, When, What)
 #JWachter, 2019-02-16, Created File
 #JWachter, 2019-02-16, Updated create report function to split data processing and data presentation. Now have 2 funcs
-#JWachter, 2019-02-10, looked to use list comprehension, didn't find a good spot
+#JWachter, 2019-02-16, Added two new helper functions to print an email and add donor info to the database, pulling them out of the send thank you function
 
 
 import sys
@@ -14,31 +14,38 @@ donor_db = {"William Gates, III": [653772.32, 12.17],
                         "Me Myself": [100]}
 user_prompt = "\n".join(("Welcome to your Donor Database", "Please choose an option: ", "1 - Create One 'Thank You' Letter", "2 - Create a Report", "3 - Create Thank You letters for all donors", "4 - Quit\n"))
 
-def sendthankyou(database = donor_db):
+def sendthankyou(database=donor_db):
     user_input = ""
     donorlist = []
-    while user_input != "Main Menu":
-        try:
-            user_input = input("Let's send some Thank You letters.\nType 'list' to see a list of donors, or input the donors full name to add a gift and send a Thank You letter. To return to the main menu, type 'Main Menu': ")
-            if user_input in donor_db:
-                amtdonated = int(input("Please enter the amount donated: "))
-                database[user_input].append(amtdonated)
-                thankyouletterdict = {"Name": user_input, "Amount": amtdonated}
-                print("+"*45 + "\nNice!\n Thanks, {Name} for the ${Amount}!\n\nSincerely, me\n".format(**thankyouletterdict) + "+"*45)
-            elif user_input == "list":
-                for donor in database:
-                    donorlist.append(donor)
-                    print(donor + "\n")
-            elif user_input not in database and user_input != 'Main Menu':
-                database[user_input] = []
-                print("Great, {} has been added to the donor database.".format(user_input))
-                amtdonated = int(input("Please enter the amount donated by {}: ".format(user_input)))
-                database[user_input].append(amtdonated)
-                thankyouletterdict = {"Name": user_input, "Amount": amtdonated}
-                print("+" * 45 + "\nNice! Thanks, {Name} for the ${Amount}!\n\nSincerely, me\n".format(**thankyouletterdict) + "+" * 45)
-        except ValueError as ve:
-            print('please enter a valid number', type(ve))
+    while user_input.lower() != "Main Menu":
+        user_input = input(
+            "Let's send some Thank You letters.\nType 'list' to see a list of donors, or input the donors full name to add a gift and send a Thank You letter. To return to the main menu, type 'Main Menu': ")
+        if user_input == "list":
+            for donor in database:
+                donorlist.append(donor)
+                print(donor + "\n")
+        elif user_input != "Main Menu":
+            amtdonated = int(input("Please enter the amount donated: "))
+            adddonation(user_input, amtdonated, database)
+            print(tyemail(user_input, amtdonated))
+        else:
+            main()
 
+def adddonation(userinput, donationamount, database=donor_db):
+    try:
+        if userinput in database:
+            database[userinput].append(donationamount)
+        else:
+            database[userinput] = [donationamount]
+    except ValueError as ve:
+        print('Please enter a valid number', type(ve))
+    return database
+
+def tyemail(user_input, amtdonated):
+    thankyouletterdict = {"Name": user_input, "Amount": amtdonated}
+    print("+" * 45 + "\nNice!\n Thanks, {Name} for the ${Amount}!\n\nSincerely, me\n".format(
+        **thankyouletterdict) + "+" * 45)
+    
 def createreport(database = donor_db):
     try:
         reportstring = str("        Here is your report\n" + "="*65 + "\n" + "Donor Name          |  Total Given  | Num Gifts | Average Gift\n" +
@@ -55,14 +62,17 @@ def displayreport():
     print("\n")
 
 def tylettertxt():
+    listtxt = {}
     for donor in donor_db:
-        txt = "+"*45 + "\nNice!\n\nThanks, {} for the ${}! You have given ${} since you started donating.\n\nSincerely, me\n".format(donor, donor_db[donor][-1], sum(donor_db[donor])) + "+"*45
-    return txt
+    #txt = "+"*45 + "\nNice!\n\nThanks, {} for the ${}! You have given ${} since you started donating.\n\nSincerely, me\n".format(donor, donor_db[donor][-1], sum(donor_db[donor])) + "+"*45
+        listtxt.update({donor: "+"*45 + "\nNice!\n\nThanks, {} for the ${}! You have given ${} since you started donating.\n\nSincerely, me\n".format(donor, donor_db[donor][-1], sum(donor_db[donor])) + "+"*45})
+    print(listtxt)
+    return listtxt
 
 def createthankyouletters(txt = tylettertxt()):
     for donor in donor_db:
         with open('/Users/John/Python210-W19/students/johnwachter/mailroom/{}_letter.txt'.format(donor), 'w') as donorletter:
-            donorletter.write(txt)
+                donorletter.write(txt)
     print("Letters generated.\n")
 
 
@@ -74,6 +84,8 @@ def responsedict(choice):
     casedict = {'1': sendthankyou, '2': displayreport, '3': createthankyouletters, '4': quitprogram}
     if casedict.get(choice) == "1":
         return casedict.get(choice)
+    elif casedict.get(choice) == "3":
+        return casedict.get(choice(tylettertxt()))
     else:
         return casedict.get(choice)()
     main()

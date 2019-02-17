ecc831d2f32bcbfb4405cddb818789a4dbb58090
johnwachter <johnwachter29@gmail.com>
2019-02-16 21:26:02 -0800

    added test to check if the adddonation function properly adds a new donor and amount to a database

--- students/johnwachter/mailroom/test_mailroom4.py
+++ students/johnwachter/mailroom/test_mailroom4.py
@@ -2,8 +2,9 @@ from os import path
 from mailroom4 import createreport
 from mailroom4 import displayreport
 from mailroom4 import tylettertxt
+from mailroom4 import adddonation
 
-newdb= {"Me":[2,2]}
+testdb = {}
 
 fullpath1 = r"C:\\Users\\John\\Python210-W19\\students\\johnwachter\\mailroom\\Me Myself_letter.txt"
 fullpath2 = r"C:\\Users\\John\\Python210-W19\\students\\johnwachter\\mailroom\\Jeff Bezos_letter.txt"
@@ -12,11 +13,6 @@ fullpath4 = r"C:\\Users\\John\\Python210-W19\\students\\johnwachter\\mailroom\\P
 fullpath5 = r"C:\\Users\\John\\Python210-W19\\students\\johnwachter\\mailroom\\William Gates, III_letter.txt"
 
 def test_1():
-    assert createreport(newdb) == 2
-    print("test_1 passed")
-
-
-def test_2():
     assert path.exists(fullpath1)
     print("The file ending with {} does exist | Pass".format(fullpath1[-20:]))
     assert path.exists(fullpath2)
@@ -28,6 +24,11 @@ def test_2():
     assert path.exists(fullpath5)
     print("The file ending with {} does exist | Pass".format(fullpath5[-20:]))
 
+def test_2():
+    assert adddonation("Me Myself", "900", database = testdb) == {"Me Myself":['900']}
+    print("passed")
+
+test_1()
 test_2()
 
 
